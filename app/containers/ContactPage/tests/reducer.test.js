d83e1f5bf1c0a48eb6c49f2d2ad0e869a11c3fa7
Victoria Kurzweg <vkurzweg@gmail.com>
2017-11-17 23:13:08 -0800

    remove old components

--- app/containers/ContactPage/tests/reducer.test.js
+++
@@ -1,9 +0,0 @@
-
-import { fromJS } from 'immutable';
-import contactPageReducer from '../reducer';
-
-describe('contactPageReducer', () => {
-  it('returns the initial state', () => {
-    expect(contactPageReducer(undefined, {})).toEqual(fromJS({}));
-  });
-});
