0dcb5e6ca3afd6804e81a5dce90e9ab955a93d07
Victoria Kurzweg <vkurzweg@gmail.com>
2017-11-18 13:39:48 -0800

    add contact info

--- app/components/ContentMobile/index.js
+++ app/components/ContentMobile/index.js
@@ -12,15 +12,15 @@ import { Image } from 'cloudinary-react';
 function ContentDesktop() {
   return (
     <div className="container" style={{ width: '100%', padding: '0', backgroundColor: '#141213', zIndex: '5' }}>
-      <div className="row" style={{ margin: '0 auto', width: '100%', marginTop: '15%', backgroundColor: '#DEED96' }}>
+      <div className="row" style={{ margin: '0 auto', width: '100%', marginTop: '10%', backgroundColor: '#DEED96' }}>
         <div style={{ padding: '2%' }}>
-          <h1 style={{ fontFamily: 'Ubuntu', fontSize: '22px', textAlign: 'center', textTransform: 'uppercase', letterSpacing: '5px' }}>coming soon</h1>
+          <h2 style={{ fontFamily: 'Ubuntu', fontSize: '22px', textAlign: 'center', textTransform: 'uppercase', letterSpacing: '5px' }}>coming soon</h2>
         </div>
       </div>
       <div style={{ backgroundColor: '#FAFAFA' }}>
-        <Image style={{ display: 'block', margin: '0 auto', paddingTop: '10%', paddingBottom: '10%' }} cloudName="kurzweg" publicId="casey_wireframe" width="200" quality="auto" crop="scale" responsive />
+        <Image style={{ display: 'block', margin: '0 auto', paddingTop: '10%', paddingBottom: '10%' }} cloudName="kurzweg" publicId="casey_wireframe" width="150" quality="auto" crop="scale" responsive />
       </div>
-      <div className="row" style={{ margin: '0 auto', width: '100%', backgroundColor: '#DEED96', marginBottom: '35%' }}>
+      <div className="row" style={{ margin: '0 auto', width: '100%', backgroundColor: '#DEED96', marginBottom: '5%' }}>
         <div className="col-xs-2" style={{ padding: '2%' }}>
           <Image style={{ display: 'block', margin: '0 auto', paddingTop: '4%' }} cloudName="kurzweg" publicId="casey_star" width="40" quality="auto" crop="scale" responsive />
         </div>
@@ -31,6 +31,7 @@ function ContentDesktop() {
           <Image style={{ display: 'block', margin: '0 auto', paddingTop: '4%' }} cloudName="kurzweg" publicId="casey_star" width="40" quality="auto" crop="scale" responsive />
         </div>
       </div>
+      <h3 style={{ textAlign: 'center', fontFamily: 'Ubuntu', color: 'white', textTransform: 'uppercase', fontSize: '16px', paddingTop: '2%', paddingBottom: '5%', letterSpacing: '3px'}}>contact: casey@caseyahern.com</h3>
     </div>
   );
 }
