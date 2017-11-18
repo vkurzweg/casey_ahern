e331d20170a7e7442ca930a6a8a7d6327a36a940
Victoria Kurzweg <vkurzweg@gmail.com>
2017-11-18 13:57:22 -0800

    adjust styling - mobile

--- app/components/ContentMobile/index.js
+++ app/components/ContentMobile/index.js
@@ -18,7 +18,7 @@ function ContentDesktop() {
         </div>
       </div>
       <div style={{ backgroundColor: '#FAFAFA' }}>
-        <Image style={{ display: 'block', margin: '0 auto', paddingTop: '5%', paddingBottom: '2%' }} cloudName="kurzweg" publicId="casey_wireframe" width="150" quality="auto" crop="scale" responsive />
+        <Image style={{ display: 'block', margin: '0 auto', paddingTop: '5%', paddingBottom: '2%' }} cloudName="kurzweg" publicId="casey_wireframe" width="200" quality="auto" crop="scale" responsive />
       </div>
       <div className="row" style={{ margin: '0 auto', width: '100%', backgroundColor: '#DEED96', marginBottom: '5%' }}>
         <div className="col-xs-2" style={{ padding: '2%' }}>
@@ -31,7 +31,7 @@ function ContentDesktop() {
           <Image style={{ display: 'block', margin: '0 auto', paddingTop: '4%' }} cloudName="kurzweg" publicId="casey_star" width="40" quality="auto" crop="scale" responsive />
         </div>
       </div>
-      <h3 style={{ textAlign: 'center', fontFamily: 'Ubuntu', color: 'white', textTransform: 'uppercase', fontSize: '16px', paddingTop: '2%', paddingBottom: '5%', letterSpacing: '3px'}}>contact: casey@caseyahern.com</h3>
+      <h3 style={{ textAlign: 'center', fontFamily: 'Ubuntu', color: 'white', textTransform: 'uppercase', fontSize: '16px', paddingTop: '2%', paddingBottom: '10%', letterSpacing: '3px'}}>contact: casey@caseyahern.com</h3>
     </div>
   );
 }
